7a7e939e0904f01528160cf9e0b6eae13ba3f4fa
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-02-15 20:35:36 -0500

    set cars table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increment('id')
+    tbl.string('vin', 17).notNullable().unique()
+    tbl.string('make', 128).notNullable()
+    tbl.string('model', 128).notNullable()
+    tbl.numeric('milage').notNullable()
+    tbl.string('title', 128 )
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
