b2073c45f428db4c55a1e31040db538e2e9e864f
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-02-15 20:16:59 -0500

    set seed data and export

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+const cars = [
+    {
+        vin: '111111111111',
+        make: 'toyota',
+        model: 'prius',
+        milage: 5005,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        milage: 115265,
+        title: 'salvage'
+    },
+    {
+        vin: '111111111111',
+        make: 'ford',
+        model: 'focus',
+        milage: 11321
+    }
+]
+
+exports.seed = function(knex){
+    return knex('cars')
+    .truncate().then(()=>{
+        return knex('cars').insert(cars)
+    })
+}
+
+// async way of exporting
+// exports.seed = async function(knex){
+//     await knex('cars').truncate()
+//     await knex('cars').insert(cars)
+// }
